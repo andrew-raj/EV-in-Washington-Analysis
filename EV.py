59384e88a9def13a5d5984df358a2460f197132a
andrew-raj <andrew.rajan03@gmail.com>
2025-04-15 13:23:15 -0500

    Update [EV-in-Washington-Analysis] with new changes

--- EV.py
+++ EV.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 csv_path = 'Electric_Vehicle_Population_Data.csv'
@@ -13,7 +14,14 @@ maxMake = df['Make'].value_counts().idxmax()
 model = df['Model'].value_counts()
 popModel = df['Model'].value_counts().idxmax()
 
+makes.plot(kind = 'pie',autopct = '%1.1f%%',startangle=0,figsize=(10,10))
+
 
 print('The make of the vehicles range from', min, 'to', max)
-print("The car brand with the most was", maxMake, "with", makes.max(), "out of the", population, "cars.")
+print("The car brand with the most was", maxMake, "with", makes.max(), "out of the", population, "cars. This is", str(round((makes.max()/population)*100,1)) + "% ", "of the cars.")
 print('The most popular model was the', popModel + ", which was", model.max(), "of the", makes.max(), "from", maxMake)
+
+
+plt.title('EV Makes')
+plt.ylabel('')
+plt.show()
